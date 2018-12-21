f8d79ef0455f16541ead321310c25429c9b68ce3
WuJinchuan <455134118@qq.com>
2018-12-21 21:06:37 +0800

    refine search-route page

--- frontend/src/h5/modules/core/controllers/search-route.js
+++ frontend/src/h5/modules/core/controllers/search-route.js
@@ -1,5 +1,21 @@
 angular.module(
     'app'
-  ).controller('core.SearchRouteController', ['$scope', '$rootScope', '$http', 'restService', '$cookies', '$stateParams', function ($scope, $rootScope, $http, restService, $cookies, $stateParams) {
-    
+  ).controller('core.SearchRouteController', ['$scope', '$rootScope', '$http', 'restService', '$cookies', '$stateParams', '$timeout', function ($scope, $rootScope, $http, restService, $cookies, $stateParams, $timeout) {
+    $timeout(() => {
+      swiper = new Swiper('.search-filter-container', {
+        //direction: 'horizontal',
+        //observer: true,
+        //observerParents: true,
+        effect : 'flip',
+        flipEffect: {
+          slideShadows : true,
+          limitRotation : true,
+        },
+        pagination : '.swiper-pagination',
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }
+      })
+    }, 100)
   }])
